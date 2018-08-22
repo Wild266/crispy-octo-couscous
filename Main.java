479099d8af7e12301e213a76464621748e6ca108
Wild266 <32823036+Wild266@users.noreply.github.com>
2018-08-21 22:22:08 -0400

    Added enginespeed question

--- Main.java
+++ Main.java
@@ -61,6 +61,12 @@ public class Main extends Askings{
 			System.out.println("\n\nYOU DIED!");
 			System.exit(0);
 		}
+		if (!h("You decide to start driving to the ship. Should you drive fast?",
+				"You push the throttle lever up as far as it goes.",
+				"The boat crawls so slowly you can't tell if it's the waves moving the boat or the engine. Th dragon seems to get curios in the \ncabin before meeting you inside.",s,p,i)) {
+			System.out.println("\n\nYOU DIED!");
+			System.exit(0);
+		}
 		System.out.println("\n\nCongratulations! You Win!");
 		System.exit(0);
 		
