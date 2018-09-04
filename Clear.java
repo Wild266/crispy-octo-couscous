a80532c2220fe1db7aefeb4c0ea77dfd5430c2af
Wild266 <32823036+Wild266@users.noreply.github.com>
2018-09-04 19:01:04 -0400

    Method not functional and commented out in main
    This method will not work unless run through console

--- Clear.java
+++ Clear.java
@@ -1,7 +1,8 @@
-
+//The purpose of this was to clear the console for a more user friendly GUI
 public class Clear {
+	//This method isn't functional unless you are using the command window of your PC
 	public static void clearScreen() {  
-	    System.out.print("\033[H\033[2J");  
+	    System.out.print("\033[H\033[2J");  //Still wondering what this piece of code means, it was discovered on Stack Overflow
 	    System.out.flush();  
 	}  
 }
